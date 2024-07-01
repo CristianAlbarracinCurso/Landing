8d47ac2a8830dbdc59886c40a1b3877cf6ce8d3c
Cristian Albarracin <denwa_cda@hotmail.com>
2024-07-01 20:30:47 -0300

    first commit

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+    
+<div className="container justify-content-between align-items-center">
+ <h5>{props.mensaje}</h5>  
+</div>
+
+  )
+}
+
+export default ItemListContainer
